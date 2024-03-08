eaa2efd9ccc20fd5d5dfb34634dcf1c851934f89
Nquartey17 <niikwarteiq@gmail.com>
2024-03-08 09:46:02 -0500

    JavaScript expressions challenge completed

---
+++ javascript-expressions-challenge/src/index.js
@@ -0,0 +1,23 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
+import React from "react";
+import ReactDOM  from "react-dom";
+
+const YOURNAME = "Nii-Kwartei Quartey";
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+ReactDOM.render(
+<div>
+<p>Created by {YOURNAME}</p>
+<p>Copyright {year}</p>
+</div>,
+document.getElementById("root")
+);
