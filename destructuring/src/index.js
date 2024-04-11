9e28be4ebc128cb4f99c8f0c306be6da953c4564
Nquartey17 <niikwarteiq@gmail.com>
2024-04-11 13:20:56 -0400

    Destructuring practice completed

---
+++ destructuring/src/index.js
@@ -0,0 +1,46 @@
+// CHALLENGE: uncomment the code below and see the car stats rendered
+
+// import animals, { useAnimals } from "./data";
+
+// console.log(animals);
+
+// const [cat, dog] = animals;
+// console.log(cat);
+
+//Object names must be the same as variable name, or rename them with :
+// const { name: catName, sound: catSound } = cat;
+
+// const { name = "Fluffy", sound = "Purr" } = cat;
+
+// const {
+//   name,
+//   sound,
+//   feedingRequirements: { food, water },
+// } = cat;
+// console.log(food);
+
+// const [animal, makeSound] = useAnimals(cat);
+// console.log(animal);
+// makeSound();
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// ReactDOM.render(
+//   <table>
+//     <tr>
+//       <th>Brand</th>
+//       <th>Top Speed</th>
+//     </tr>
+//     <tr>
+//       <td>{tesla.model}</td>
+//       <td>{teslaTopSpeed}</td>
+//       <td>{teslaTopColour}</td>
+//     </tr>
+//     <tr>
+//       <td>{honda.model}</td>
+//       <td>{hondaTopSpeed}</td>
+//       <td>{hondaTopColour}</td>
+//     </tr>
+//   </table>,
+//   document.getElementById("root")
+// );
